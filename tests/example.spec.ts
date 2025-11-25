790056f153f9dbcaaa02bf634ad1c02a01fb109a
Nataliia Kindrakevych <Kindrakevych.natali@gmail.com>
2025-11-25 13:57:42 +0000

    Create repo for test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,16 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+//hook - describe for group of tests
+test.describe('navigation', () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto('https://playwright.dev/');
+  });
+
+  test('main navigation', async ({ page }) => {
+    // Assertions use the expect API.
+    await expect(page).toHaveURL('https://playwright.dev/');
+  });
+});
